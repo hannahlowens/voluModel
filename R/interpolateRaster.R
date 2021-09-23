3b3b0b01e91a3f9cd295a5475027a75259a456b1
hannahlowens <hannah.owens@gmail.com>
2021-09-23 13:13:19 +0200

    add fastTps capability to interpolateRaster

--- R/interpolateRaster.R
+++ R/interpolateRaster.R
@@ -1,27 +1,96 @@
 #' @title Interpolate patchily sampled rasters
 #'
 #' @description Uses thin plate spline regression from
-#' `fields` package to interpolate missing raster values.
-#' Note that this is only done in two dimensions, x and y.
+#' `fields` package to interpolate missing two-dimensional
+#' raster values.
 #'
 #' @param inputRaster An object of class `raster`
+#' @param fast A logical operator. Setting to `T` triggers use
+#' of `fastTps` instead of `Tps`.
+#' @param ... For any additional arguments passed to `Tps` or `fastTps`
+#'
+#' @details Missing data values from original raster
+#' are replaced with interpolated values. User has the
+#' option of choosing `fastTps` to speed calculation,
+#' but be advised that this is only an approximation
+#' of a true thin plate spline.
 #'
 #' @return An object of class raster
 #'
 #' @examples
+#' library(raster)
+#' library(fields)
 #' # Create sample raster
-#' r <- raster(ncol=10, nrow=10)
-#' values(r) <- 1:100
+#' r <- raster(ncol=100, nrow=100)
+#' values(r) <- 1:10000
 #'
 #' # Introduce a "hole"
 #' values(r)[67:70] <- NA
+#'
+#' # Patch hole with interpolateRaster
 #' interpolatedRaster <- interpolateRaster(r)
+#' fastInterp <- interpolateRaster(r, fast = TRUE, aRange = 3.0)
+#'
+#' @import raster
+#' @import fields
+#'
+#' @seealso \code{\link[fields]{Tps}}, \code{\link[fields]{fastTps}}
 #'
 #' @keywords dataPrep
 #' @export
 
 # Interpolates patchily sampled rasters (2d)
-interpolateRaster <- function(inputRaster){
+interpolateRaster <- function(inputRaster, fast = F, ...){
+  args <- list(...)
+
+  if("lon.lat" %in% names(args)){
+    lon.lat <- args$lon.lat
+  } else{
+    lon.lat <- FALSE
+  }
+
+  if("aRange" %in% names(args)){
+    aRange <- args$aRange
+  } else{
+    aRange <- NA
+  }
+
+  if("theta" %in% names(args)){
+    aRange <- args$theta # Theta and aRange are same, per fields docs
+  }
+
+  if("REML" %in% names(args)){
+    REML <- args$REML
+  } else{
+    REML <- FALSE
+  }
+
+  # Input error check
+  if (!class(inputRaster) == "RasterLayer") {
+    warning(paste0("inputRaster is not of class 'RasterLayer'.\n"))
+    return(NULL)
+  }
+  if (!is.logical(fast)) {
+    warning(paste0("Argument 'fast' is not of type 'logical'.\n"))
+    return(NULL)
+  }
+  if (!is.logical(lon.lat)) {
+    warning(paste0("Argument 'lon.lat' is not of type 'logical'.\n"))
+    return(NULL)
+  }
+  if(fast && is.na(aRange)){
+    warning(paste0("'aRange' must be specified when using `fastTps()`.\n"))
+    return(NULL)
+  }
+  if(fast && !is.numeric(aRange)){
+    warning(paste0("'aRange' must be numeric.\n"))
+    return(NULL)
+  }
+  if (!is.logical(REML)) {
+    warning(paste0("Argument 'REML' is not of type 'logical'.\n"))
+    return(NULL)
+  }
+
   # Prepare input data
   r <- inputRaster
   ra <- aggregate(r, 4)
@@ -33,7 +102,12 @@ interpolateRaster <- function(inputRaster){
   v <- v[i]
 
   #### Thin plate spline model
-  tps <- Tps(xy, v, lon.lat = T, verbose = F)
+  if(fast){
+    tps <- fastTps(xy, v, lon.lat = lon.lat, aRange = aRange, REML = REML, verbose = F)
+  } else {
+    tps <- Tps(xy, v, lon.lat = lon.lat, verbose = F)
+  }
+
   p <- raster(r)
 
   # use model to predict values at all locations
