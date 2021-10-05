181f868099d2e4845c527b14a412646d0ee8178c
hannahlowens <hannah.owens@gmail.com>
2021-10-05 15:57:49 +0200

    Added a Tps-based raster smoother

--- tests/testthat/test-interpolateRaster.R
+++ tests/testthat/test-interpolateRaster.R
@@ -1,10 +1,14 @@
 library(raster)
 library(fields)
 
+# interpolateRaster() tests ----
+# Create sample raster
+r <- raster(ncol=10, nrow=10)
+values(r) <- 1:100
+
+completeRaster <- r
+
 test_that("interpolateRaster input warnings behave as expected", {
-  # Create sample raster
-  r <- raster(ncol=10, nrow=10)
-  values(r) <- 1:100
 
   # Tests
   expect_error(interpolateRaster())
@@ -21,29 +25,16 @@ test_that("interpolateRaster input warnings behave as expected", {
                                    REML = "a"))
 })
 
+# Introduce a "hole"
+values(r)[67:70] <- NA
+
 test_that("interpolateRaster returns appropriate information", {
-  # Create sample raster
-  r <- raster(ncol=10, nrow=10)
-  values(r) <- 1:100
-
-  # Introduce a "hole"
-  values(r)[67:70] <- NA
-
-  # Execute function
+ # Execute function
   interpolatedRaster <- interpolateRaster(r)
   expect_equal(class(r), class(interpolatedRaster))
 })
 
 test_that("interpolateRaster interpolates as expected", {
-
-  # Create sample raster
-  r <- raster(ncol=10, nrow=10)
-  values(r) <- 1:100
-  completeRaster <- r
-
-  # Introduce a "hole"
-  values(r)[67:70] <- NA
-
   # Execute function
   interpolatedRaster <- interpolateRaster(r)
 
@@ -55,38 +46,89 @@ test_that("interpolateRaster interpolates as expected", {
                cellStats(interpolatedRaster, stat = min))
 })
 
+# Create sample raster
+r <- raster(ncol=100, nrow=100)
+values(r) <- 1:10000
+completeRaster <- r
+
+# Introduce a "hole"
+values(r)[100:105] <- NA
+
+# Execute function
+interpolatedRaster <- interpolateRaster(r, fast = T, aRange = 1)
+
 test_that("fast version of interpolateRaster returns appropriate information", {
-  # Create sample raster
-  r <- raster(ncol=100, nrow=100)
-  values(r) <- 1:10000
-  completeRaster <- r
-
-  # Introduce a "hole"
-  values(r)[100:105] <- NA
-
-  # Execute function
-  interpolatedRaster <- interpolateRaster(r, fast = T, aRange = 1)
-
   expect_equal(class(r), class(interpolatedRaster))
 })
 
 test_that("fast version of interpolateRaster interpolates as expected", {
-
-  # Create sample raster
-  r <- raster(ncol=100, nrow=100)
-  values(r) <- 1:10000
-  completeRaster <- r
-
-  # Introduce a "hole"
-  values(r)[100:105] <- NA
-
-  # Execute function
-  interpolatedRaster <- interpolateRaster(r, fast = T, aRange = 1)
-
   # Examine results
   expect_equal(cellStats(is.na(interpolatedRaster), sum), 0)
   expect_equal(cellStats(completeRaster, stat = max),
                cellStats(interpolatedRaster, stat = max))
   expect_equal(cellStats(completeRaster, stat = min),
                cellStats(interpolatedRaster, stat = min))
+})
+
+# smoothRaster() tests ----
+# Create sample raster
+r <- raster(ncol=100, nrow=100)
+values(r) <- 1:10000
+
+# Introduce a "bubble"
+values(r)[820:825] <- 9999
+
+test_that("smoothRaster input warnings behave as expected", {
+
+  # Tests
+  expect_error(smoothRaster())
+  expect_warning(smoothRaster(inputRaster = "a"))
+  expect_warning(smoothRaster(inputRaster = r, fast = "a"))
+  expect_warning(smoothRaster(inputRaster = r, fast = F,
+                                   lon.lat = "a"))
+  expect_warning(smoothRaster(inputRaster = r, fast = T))
+  expect_warning(smoothRaster(inputRaster = r, fast = T,
+                                   aRange = "a"))
+  expect_warning(smoothRaster(inputRaster = r, fast = T,
+                                   theta = "a"))
+  expect_warning(smoothRaster(inputRaster = r, fast = T,
+                                   REML = "a"))
+})
+
+# Execute function
+smoothedRaster <- smoothRaster(r)
+
+test_that("smoothRaster returns appropriate information", {
+  expect_equal(class(r), class(smoothedRaster))
+})
+
+test_that("smoothRaster interpolates as expected", {
+  # Examine results
+  expect_equal(cellStats(is.na(smoothedRaster), sum), 0)
+  expect_equal(cellStats(r, stat = max),
+               round(cellStats(smoothedRaster, stat = max), digits = 0))
+})
+
+# Create sample raster
+r <- raster(ncol=100, nrow=100)
+values(r) <- 1:10000
+completeRaster <- r
+
+# Introduce a "hole"
+values(r)[100:105] <- 200
+
+# Execute function
+smoothedRaster <- smoothRaster(r, fast = T, aRange = 1)
+
+test_that("fast version of smoothRaster returns appropriate information", {
+  expect_equal(class(r), class(smoothedRaster))
+})
+
+test_that("fast version of smoothRaster interpolates as expected", {
+  # Examine results
+  expect_equal(cellStats(is.na(smoothedRaster), sum), 0)
+  expect_equal(cellStats(smoothedRaster, stat = max),
+               cellStats(smoothedRaster, stat = max))
+  expect_equal(cellStats(smoothedRaster, stat = min),
+               cellStats(smoothedRaster, stat = min))
 })
