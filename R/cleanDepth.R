fcfb1aefc89ff2bac1dac107cacfb3500bb0b625
Emmaline <emmalinesheahan@gmail.com>
2025-04-25 12:57:50 -0400

    Edited cleanDepth function to fix word order error, matchDepth function to match depths in occs to names of depths in spatRaster stack

--- R/cleanDepth.R
+++ R/cleanDepth.R
@@ -1,27 +1,27 @@
-#' @title clean and flag coordinates based on intersection with a land polygon, 
+#' @title Clean and flag coordinates based on intersection with a land polygon,
 #' intersection with a bathymetry layer, and/or by a given depth range
 #'
-#' @description function to remove or flag coordinates on land, coordinates which are deeper 
-#' than the known bathymetry at a given xy location, and/or coordinates outside of 
+#' @description function to remove or flag coordinates on land, coordinates which are deeper
+#' than the known bathymetry at a given xy location, and/or coordinates outside of
 #' a specified depth range when cleaning coordinates for species in marine environments
 #'
 #' @param occs dataframe containing columns named "longitude", "latitude", and "depth," where
 #' depth values are positive
-#' @param bathy a spatRaster of bathymetry in the same units as the occurrence depth, where 
+#' @param bathy a spatRaster of bathymetry in the same units as the occurrence depth, where
 #' values are negative
 #' @param land_poly optional polygon of continents in same projection as occurrence data
 #' @param depth_range optional range of depth values in the form of c(upper, lower) in which
 #' all points should fall between. depth value should be positive, as in the case of the occs
-#' @param flag default is F, if T, points are not removed, but flagged if they intersect, and a 
+#' @param flag default is F, if T, points are not removed, but flagged if they intersect, and a
 #' column is added to the output indicating what points have been flagged
 #' @param bottom_correct default is F, but if T, points are not removed, and a column is added
 #' to the output displaying the bathymetry value at the flagged point for corrective purposes
 #'
-#' @details assumes depths are listed as positive in the occs and depth_range, while the 
+#' @details assumes depths are listed as positive in the occs and depth_range, while the
 #' bathymetry layer lists them as negative when below the sea surface
-#' 
+#'
 #' @return an object of class dataframe containing cleaned or flagged occurrences
-#' 
+#'
 #' @examples
 #' library(terra)
 #' library(dplyr)
@@ -29,34 +29,34 @@
 #' # Create sample bathymetry
 #' r1 <- rast(ncol=10, nrow=10)
 #' values(r1) <- c(-100:0)
-#' 
+#'
 #' # Create test occurrences
 #' set.seed(0)
 #' longitude <- sample(ext(r1)[1]:ext(r1)[2], size = 10, replace = FALSE)
 #' set.seed(0)
 #' latitude <- sample(ext(r1)[3]:ext(r1)[4], size = 10, replace = FALSE)
 #' set.seed(0)
-#' depth <- sample(1:100, size = 10, replace = TRUE)                    
+#' depth <- sample(1:100, size = 10, replace = TRUE)
 #' occs <- data.frame(longitude, latitude, depth)
-#' 
+#'
 #' #Heres the function
 #' result <- cleanDepth(bathy = r1, occs = occs)
-#' 
+#'
 #' @importFrom dplyr select
-#' @importFrom extract terra
+#' @importFrom terra extract
 #' @import sf
-#' 
+#'
 #' @keywords occurrence cleaning
-#' 
+#'
 #' @export
 
-cleanDepth <- function(occs, bathy, land_poly, depth_range, flag = FALSE, 
+cleanDepth <- function(occs, bathy, land_poly, depth_range, flag = FALSE,
                        bottom_correct = FALSE) {
-  
+
   # bathymetry cleaning
   occ_mat <- occs %>% dplyr::select(longitude, latitude)
   depth_at_points <- terra::extract(bathy, occ_mat)
-  occs$depth[which(occs$depth > 0 & !(is.na(occs$depth)))] <- 0 - 
+  occs$depth[which(occs$depth > 0 & !(is.na(occs$depth)))] <- 0 -
     occs$depth[which(occs$depth > 0 & !(is.na(occs$depth)))]
   bath_flag <- vector(length = nrow(occs))
   for (k in 1:nrow(occs)) {
@@ -86,7 +86,7 @@ cleanDepth <- function(occs, bathy, land_poly, depth_range, flag = FALSE,
     output_df <- occs[-which(bath_flag > 0),]
     print(paste0("removed ", length(which(bath_flag > 0)), " points intersecting bathymetry"))
   }
-  
+
   # in the case that land_poly is supplied
   if(!(missing(land_poly))) {
     x_sf <- st_as_sf(output_df, coords = c("longitude", "latitude"), crs = st_crs(land_poly))
@@ -109,7 +109,7 @@ cleanDepth <- function(occs, bathy, land_poly, depth_range, flag = FALSE,
       print("no points found on land")
     }
   }
-  
+
   # in the case depth_range is supplied
   if(!(missing(depth_range))) {
     rem1 <- which(!(output_df$depth <= min(depth_range)))
@@ -122,7 +122,7 @@ cleanDepth <- function(occs, bathy, land_poly, depth_range, flag = FALSE,
     }
     print(paste0("removed ", length(rem1) + length(rem2), " points outside of depth range"))
   }
-  
+
   output_df$depth <- abs(output_df$depth)
   return(output_df)
 }
